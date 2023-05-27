17731f79a00b55a73e690ffd241485f3ac384158
Владимир Ходаков <vova090666@gmail.com>
2023-05-27 13:08:42 +0300

    Автоформатирован код, настроен .gitignore

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 
 public class Main {
     public static void main(String[] args) {
-       int amount;
-       int mile;
-       amount = 24466;
-       mile = amount/20;
-       int bonus;
-       bonus=mile;
-System.out.println("Бонусыне мили: " + bonus);
+        int amount;
+        int mile;
+        amount = 24466;
+        mile = amount / 20;
+        int bonus;
+        bonus = mile;
+        System.out.println("Бонусыне мили: " + bonus);
 
 
     }
