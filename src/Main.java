28246ab9bcb7612d9c17da5599766fe237cad1eb
Владимир Ходаков <vova090666@gmail.com>
2023-05-31 15:22:07 +0300

    Убрано дублирование переменных

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 
 public class Main {
     public static void main(String[] args) {
-        int amount;
-        int mile;
-        amount = 24466;
-        mile = amount / 20;
+        int amount = 24466;
+        int mile = amount / 20;
         System.out.println("Бонусыне мили: " + mile);
     }
 }
